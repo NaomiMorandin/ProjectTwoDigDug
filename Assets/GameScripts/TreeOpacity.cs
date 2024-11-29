77dfb3bc114dcd8522349457a4439c8aaca9d493
SAE1\1034391 <1034391@student.sae.edu.au>
2024-11-29 13:42:39 +1030

    Added key sprite, added lights to game objects, finalised tree opacity script

--- Assets/GameScripts/TreeOpacity.cs
+++ Assets/GameScripts/TreeOpacity.cs
@@ -1,27 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TreeOpacity : MonoBehaviour
 {
     SpriteRenderer m_SpriteRenderer;
+    
 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
 
-        Debug.Log("hit");
+        Debug.Log("tree");
         if (other != null && other.gameObject.CompareTag("Player"))
         //defensive code, checks for null before using the perameter, failsafe
         {
-            m_SpriteRenderer = GetComponent<SpriteRenderer>();
-
-            //m_SpriteRenderer.color.a = 155f; 
+            GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, 0.2f);
         }
+
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+
+        Debug.Log("tree");
+        if (other != null && other.gameObject.CompareTag("Player"))
+        //defensive code, checks for null before using the perameter, failsafe
+        {
+            GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, 1f);
+        }
+
     }
     // Start is called before the first frame update
     void Start()
     {
+
         
 
     }
@@ -31,4 +44,6 @@ public class TreeOpacity : MonoBehaviour
     {
         
     }
+
+
 }
