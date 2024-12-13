68e281896039021e851dc68fa29830d400ab5091
SAE1\1034391 <1034391@student.sae.edu.au>
2024-12-13 13:41:16 +1030

    Revert "Merge branch 'KYEBRANCH'"
    This reverts commit 7873b1c78bacf09a4748c0c802b51442baa813ba, reversing
    changes made to 85b07ae4e25a7f181b346f0a8e56ef542b37537b.

--- Assets/GameScripts/GameManager.cs
+++ Assets/GameScripts/GameManager.cs
@@ -14,29 +14,22 @@ public class GameManager : MonoBehaviour
     public GameObject startOver;
     public int enemyCount = 0;
     public bool canMove = true;
-    GameObject[] enemiesAlive;
-    List<GameObject> enemies = new List<GameObject>();
+   
     // Start is called before the first frame update
     void Start()
     {
-        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Enemy");
-
-        foreach (GameObject go in gameObjects) { 
-            enemies.Add(go);
-        }
-
+       
         
         enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
         //enemy count is set to the number of game objects with the tag Enemy
-        
-        
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
 
 
     }
